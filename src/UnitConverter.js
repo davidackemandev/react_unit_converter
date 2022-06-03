1e5c2b338b50fbe2df8e0a845c2505007e787f80
David Ackerman <david@dvacreative.com>
2022-06-03 00:30:46 -0400

    first

---
+++ src/UnitConverter.js
@@ -0,0 +1,61 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+import data from './unitconverterdata.json';
+import './unitConverter.css';
+
+function App() {
+  const [activeTab, setActiveTab] = useState(localStorage.getItem('activeTab'));
+  const modes = data.modes;
+  useEffect(() => {
+    if (!activeTab) {
+      localStorage.setItem('activeTab', 0);
+    }
+  });
+  function clickTabHeader(e) {
+    const index = e.target.dataset.index;
+    console.log(index);
+    console.log(activeTab);
+    setActiveTab(index);
+  }
+  return (
+    <div className='App'>
+      <div className='tabBar'>
+        {modes.map((mode, index) => {
+          console.log(index);
+          return (
+            <div key={index}>
+              <div
+                className='tabHeader'
+                data-index={index}
+                onClick={(e) => {
+                  clickTabHeader(e);
+                }}
+              >
+                {mode.name}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className='formWrapper'>
+        {modes.map((mode, index) => {
+          if(index == activeTab){
+          return(
+            <div className='modeForm'>
+              hi
+              {mode.unitInputs.map((unitInput, index) => {
+                return (
+                  <div key={index} className='modeForm'>
+                    {unitInput.label}
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import UnitConverter from './UnitConverter';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <UnitConverter />
   </React.StrictMode>
 );
 
