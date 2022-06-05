5fcb18da794a0a64e87a5cd3c3d1c2a0e469f4b6
David Ackerman <david@dvacreative.com>
2022-06-05 18:32:21 -0400

    its working

--- src/UnitConverter.js
+++ src/UnitConverter.js
@@ -4,6 +4,7 @@ import data from './unitconverterdata.json';
 import './unitConverter.css';
 
 function App() {
+  const [baseValue, setBaseValue] = useState('');
   const [activeTab, setActiveTab] = useState(localStorage.getItem('activeTab'));
   const modes = data.modes;
   useEffect(() => {
@@ -13,9 +14,20 @@ function App() {
   });
   function clickTabHeader(e) {
     const index = e.target.dataset.index;
-    console.log(index);
-    console.log(activeTab);
+    setBaseValue('');
     setActiveTab(index);
+    localStorage.setItem('activeTab', index);
+  }
+  function onUnitInputChange(e, factor) {
+    const value = e.target.value;
+    setBaseValue(value / factor);
+  }
+  function calcInputValue(factor){
+    if(!baseValue){
+      return '';
+    }
+    const value = Math.round((baseValue * factor + Number.EPSILON) * 100) / 100
+    return value;
   }
   return (
     <div className='App'>
@@ -25,7 +37,7 @@ function App() {
           return (
             <div key={index}>
               <div
-                className='tabHeader'
+                className={`tabHeader ${index == activeTab ? 'active' : ''}`}
                 data-index={index}
                 onClick={(e) => {
                   clickTabHeader(e);
@@ -41,21 +53,25 @@ function App() {
         {modes.map((mode, index) => {
           if (index == activeTab) {
             return (
-              <div className='modeForm'>
-           
-                      {mode.unitInputs.map((unitInput, index) => {
-                        return (
-                         
-                            <div key={index} className='modeFormUnit'>
-                              <label for={unitInput.label}>
-                                {unitInput.label}
-                              </label>
-                              <input name={unitInput.label} />
-                            </div>
-                       
-                        );
-                      })}
-            
+              <div className='modeForm' key={index}>
+                {mode.unitInputs.map((unitInput, index) => {
+                  return (
+                    <div key={index} className='modeFormUnit'>
+                      <label
+                        htmlFor={unitInput.label}
+                        dangerouslySetInnerHTML={{ __html: unitInput.label }}
+                      />
+                      <input
+                        name={unitInput.label}
+                        value={calcInputValue(unitInput.factor)}
+                        data-factor={unitInput.factor}
+                        onChange={(e) => {
+                          onUnitInputChange(e, unitInput.factor);
+                        }}
+                      />
+                    </div>
+                  );
+                })}
               </div>
             );
           }
